412f636d6cee2fb44d6a90a9a0f461a9899ba35c
Anton Bukov <k06aaa@gmail.com>
2018-09-08 18:06:23 +0300

    Add deployer feature

--- libs/VanityEth.js
+++ libs/VanityEth.js
@@ -14,8 +14,15 @@ var isValidHex = function(hex) {
     var re = /^[0-9A-F]+$/g;
     return re.test(hex);
 }
-var isValidVanityWallet = function(wallet, input, isChecksum, isContract) {
+var isValidVanityWallet = function(wallet, input, isChecksum, isContract, isDeployer) {
     var _add = wallet.address;
+    if (isDeployer) {
+        var _deployerAdd = getDeterministicContractAddress(_add);
+        var _contractAdd = getDeterministicContractAddress(_deployerAdd, 1);
+        _contractAdd = isChecksum ? ethUtils.toChecksumAddress(_contractAdd) : _contractAdd;
+        wallet.contract = _contractAdd;
+        return _contractAdd.substr(2, input.length) == input
+    }
     if (isContract) {
         var _contractAdd = getDeterministicContractAddress(_add);
         _contractAdd = isChecksum ? ethUtils.toChecksumAddress(_contractAdd) : _contractAdd;
@@ -25,16 +32,16 @@ var isValidVanityWallet = function(wallet, input, isChecksum, isContract) {
     _add = isChecksum ? ethUtils.toChecksumAddress(_add) : _add;
     return _add.substr(2, input.length) == input;
 }
-var getVanityWallet = function(input = '', isChecksum = false, isContract = false) {
+var getVanityWallet = function(input = '', isChecksum = false, isContract = false, isDeployer = false) {
     if (!isValidHex(input)) throw new Error(ERRORS.invalidHex);
     input = isChecksum ? input : input.toLowerCase();
     var _wallet = getRandomWallet();
-    while (!isValidVanityWallet(_wallet, input, isChecksum, isContract)) _wallet = getRandomWallet(isChecksum);
+    while (!isValidVanityWallet(_wallet, input, isChecksum, isContract, isDeployer)) _wallet = getRandomWallet(isChecksum);
     if (isChecksum) _wallet.address = ethUtils.toChecksumAddress(_wallet.address);
     return _wallet;
 }
-var getDeterministicContractAddress = function(address) {
-    return '0x' + ethUtils.sha3(ethUtils.rlp.encode([address, 0])).slice(12).toString('hex');
+var getDeterministicContractAddress = function(address, nonce = 0) {
+    return '0x' + ethUtils.sha3(ethUtils.rlp.encode([address, nonce])).slice(12).toString('hex');
 }
 module.exports = {
     getVanityWallet: getVanityWallet,
