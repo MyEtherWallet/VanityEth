7d932409413ed0ee35e4a1679a4cb3cabfe26c2a
Gonçalo Sá <goncalo05@gmail.com>
2019-04-17 19:36:48 +0200

    Add CREATE2 vanity address creation

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 #! /usr/bin/env node
 
-var VanityEth = require('./libs/VanityEth');
+const VanityEth = require('./libs/VanityEth');
 const ora = require('ora');
-var cluster = require('cluster')
-var TimeFormat = require('hh-mm-ss')
-var numCPUs = require('os').cpus().length
+const cluster = require('cluster');
+const TimeFormat = require('hh-mm-ss');
+const numCPUs = require('os').cpus().length;
+const fs = require('fs');
+var ethUtils = require('ethereumjs-util');
 var argv = require('yargs')
     .usage('Usage: $0 <command> [options]')
     .example('$0 -checksum -i B00B5', 'get a wallet where address matches B00B5 in checksum format')
     .example('$0 --contract -i ABC', 'get a wallet where 0 nonce contract address matches the vanity')
+    .example('$0 --create2 0xab5801a7d398351b8be11c439e05c5b3259aec9b -b ./my_bytecode.bin', 'get a vanity address for this bytecode to be deployed by a factory at address 0xab5801(...)')
     .example('$0 -n 25 -i ABC', 'get 25 vanity wallets')
     .example('$0 -n 1000', 'get 1000 random wallets')
     .alias('i', 'input')
@@ -17,6 +20,12 @@ var argv = require('yargs')
     .alias('c', 'checksum')
     .boolean('c')
     .describe('c', 'check against the checksum address')
+    .describe('i', 'input hex string')
+    .string('create2')
+    .describe('create2', 'contract address for contract deployment with create2 opcode')
+    .alias('b', 'bytecode')
+    .string('b')
+    .describe('b', 'path to file with bytecode to be deployed with CREATE2')
     .alias('n', 'count')
     .number('n')
     .describe('n', 'number of wallets')
@@ -29,21 +38,38 @@ var argv = require('yargs')
     .alias('h', 'help')
     .epilog('copyright 2018')
     .argv;
+
 if (cluster.isMaster) {
     const args = {
         input: argv.input ? argv.input : '',
+        isCreate2: argv.create2 ? true : false,
+        create2Address: argv.create2,
+        create2Bytecode: argv.create2 ? fs.readFileSync(argv.bytecode).toString('utf-8').trim() : '',
         isChecksum: argv.checksum ? true : false,
         numWallets: argv.count ? argv.count : 1,
         isContract: argv.contract ? true : false,
         log: argv.log ? true : false,
         logFname: argv.log ? 'VanityEth-log-' + Date.now() + '.txt' : ''
     }
+    if (args.isContract && args.isCreate2) {
+        console.error('Cannot use the options "--contract" and "--create2" at the same time');
+        process.exit(1);
+    }
     if (!VanityEth.isValidHex(args.input)) {
         console.error(args.input + ' is not valid hexadecimal');
         process.exit(1);
     }
+    if (args.isCreate2) {
+        if (!VanityEth.isValidBytecode(args.create2Bytecode)) {
+            console.error(args.create2Bytecode + ' is not valid bytecode');
+            process.exit(1);
+        }
+        if (!ethUtils.isValidAddress(args.create2Address)) {
+            console.error(args.create2 + ' is not a valid address');
+            process.exit(1);
+        }
+    }
     if (args.log) {
-        var fs = require('fs');
         console.log('logging into ' + args.logFname);
         var logStream = fs.createWriteStream(args.logFname, { 'flags': 'a' });
     }
@@ -58,7 +84,10 @@ if (cluster.isMaster) {
         const worker_env = {
             input: args.input,
             isChecksum: args.isChecksum,
-            isContract: args.isContract
+            isContract: args.isContract,
+            isCreate2: args.isCreate2,
+            create2Address: args.create2Address,
+            create2Bytecode: args.create2Bytecode
         }
         proc = cluster.fork(worker_env);
         proc.on('message', function(message) {
@@ -79,13 +108,24 @@ if (cluster.isMaster) {
 
 } else {
     const worker_env = process.env;
-    while (true) {
-        process.send({
-            account: VanityEth.getVanityWallet(worker_env.input, worker_env.isChecksum == 'true', worker_env.isContract == 'true', function (){
+    if(worker_env.isCreate2) {
+        while (true) {
             process.send({
-                counter: true
-            })
-        })})
+                account: VanityEth.getVanityCreate2Address(worker_env.input, worker_env.isChecksum == 'true', worker_env.create2Address, worker_env.create2Bytecode, function (){
+                process.send({
+                    counter: true
+                })
+            })})
+        }
+    } else {
+        while (true) {
+            process.send({
+                account: VanityEth.getVanityWallet(worker_env.input, worker_env.isChecksum == 'true', worker_env.isContract == 'true', function (){
+                process.send({
+                    counter: true
+                })
+            })})
+        }
     }
 }
 process.stdin.resume();
--- libs/VanityEth.js
+++ libs/VanityEth.js
@@ -1,18 +1,33 @@
 const crypto = require('crypto');
 var ethUtils = require('ethereumjs-util');
 var ERRORS = {
-    invalidHex: "Invalid hex input"
+    invalidHex: "Invalid hex input",
+    invalidAddress: "Invalid address input",
+    invalidBytecode: "Invalid byecode input"
 }
 var getRandomWallet = function() {
     var randbytes = crypto.randomBytes(32);
     var address = '0x' + ethUtils.privateToAddress(randbytes).toString('hex');
     return { address: address, privKey: randbytes.toString('hex') }
 }
+function getRandomSalt() {
+    var randbytes = crypto.randomBytes(32);
+    return randbytes.toString('hex');
+}
 var isValidHex = function(hex) {
     if (!hex.length) return true;
-    hex = hex.toUpperCase();
-    var re = /^[0-9A-F]+$/g;
+    var re = /^(0x)?[0-9A-Fa-f]+$/g;
     return re.test(hex);
+}
+var isValidBytecode = function(bytecode) {
+    // console.log(`Yoooo, it's me: ${bytecode}`);
+    // console.log(`and my char length is: ${bytecode.length}`);
+    if (bytecode.length == 0 || bytecode.length % 2 != 0) return false;
+    var re = /^(0x)?[0-9A-Fa-f]+$/g;
+    return re.test(bytecode);
+}
+var cleanHexPrefix = function(hex) {
+    return hex.replace(/^0x/, "");
 }
 var isValidVanityWallet = function(wallet, input, isChecksum, isContract) {
     var _add = wallet.address;
@@ -25,13 +40,33 @@ var isValidVanityWallet = function(wallet, input, isChecksum, isContract) {
     _add = isChecksum ? ethUtils.toChecksumAddress(_add) : _add;
     return _add.substr(2, input.length) == input;
 }
-var getVanityWallet = function(input = '', isChecksum = false, isContract = false, counter = function(){}) {
+var getVanityWallet = function(input = '', isChecksum = false, isContract = false, ounter = function(){}) {
     if (!isValidHex(input)) throw new Error(ERRORS.invalidHex);
     input = isChecksum ? input : input.toLowerCase();
     var _wallet = getRandomWallet();
     while (!isValidVanityWallet(_wallet, input, isChecksum, isContract)) {
         counter()
-        _wallet = getRandomWallet(isChecksum);
+        _wallet = getRandomWallet();
+    }
+    if (isChecksum) _wallet.address = ethUtils.toChecksumAddress(_wallet.address);
+    return _wallet;
+}
+function isValidCreate2Address(wallet, input, creatorAddress, bytecode, isChecksum) {
+    var _address = getCreate2ContractAddress(creatorAddress, wallet.saltHex, bytecode);
+    _address = isChecksum ? ethUtils.toChecksumAddress(_address) : _address;
+    wallet.address = _address;
+    return _address.substr(2, input.length) == input
+}
+var getVanityCreate2Address = function(input = '', isChecksum = false, create2Address = '', create2Bytecode = '', counter = function(){}) {
+    if (!isValidHex(input)) throw new Error(ERRORS.invalidHex);
+    if (!ethUtils.isValidAddress(create2Address)) throw new Error(ERRORS.invalidAddress);
+    if (!isValidBytecode(create2Bytecode)) throw new Error(ERRORS.invalidBytecode);
+    input = isChecksum ? input : input.toLowerCase();
+    var _wallet = {};
+    _wallet.saltHex = getRandomSalt();
+    while (!isValidCreate2Address(_wallet, input, create2Address, create2Bytecode, isChecksum)) {
+        counter()
+        _wallet.saltHex = getRandomSalt();
     }
     if (isChecksum) _wallet.address = ethUtils.toChecksumAddress(_wallet.address);
     return _wallet;
@@ -39,8 +74,15 @@ var getVanityWallet = function(input = '', isChecksum = false, isContract = fals
 var getDeterministicContractAddress = function(address) {
     return '0x' + ethUtils.sha3(ethUtils.rlp.encode([address, 0])).slice(12).toString('hex');
 }
+function getCreate2ContractAddress (creatorAddress, saltHex, bytecode) {
+    var joinedParams = ['ff', creatorAddress, saltHex, ethUtils.sha3(bytecode).toString('hex')].map(x => x.replace(/0x/, '')).join('');
+    return `0x${ethUtils.sha3(`0x${joinedParams}`).toString('hex').slice(-40)}`.toLowerCase()
+}
 module.exports = {
     getVanityWallet: getVanityWallet,
+    getVanityCreate2Address: getVanityCreate2Address,
     isValidHex: isValidHex,
+    isValidBytecode: isValidBytecode,
+    cleanHexPrefix: cleanHexPrefix,
     ERRORS: ERRORS
 }
